554a2aced8b1fbe8890f655068dade6a2f3126ee
kristina1508 <77191183+kristina1508@users.noreply.github.com>
2021-01-22 20:06:57 +0100

    Add files via upload

---
+++ Coffie_prices2020.sql
@@ -0,0 +1,11 @@
+
+select * from Coffie_prices2020
+
+delete from Coffie_prices2020 where Date='01/14/2021' 
+
+delete from Coffie_prices2020 where Date='01/13/2021' 
+delete from Coffie_prices2020 where Date='01/12/2021'
+delete from Coffie_prices2020 where Date='01/07/2021'
+delete from Coffie_prices2020 where Date='01/06/2021'
+delete from Coffie_prices2020 where Date='01/05/2021'
+delete from Coffie_prices2020 where Date='01/04/2021'
